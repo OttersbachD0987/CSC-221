7169244f85dcd7a6c9928fa51a4823fd578481bc
OttersbachD0987 <ottersbd0987@student.faytechcc.edu>
2025-09-06 22:40:26 -0400

    Project work

--- M1Pro/M1Pro_Review_DaleyOttersbach.py
+++ M1Pro/M1Pro_Review_DaleyOttersbach.py
@@ -4,15 +4,16 @@
 # Daley Ottersbach
 
 allData = {
-    'US': {'pop':325.7, 'gdp': 19.39, 'ccy': 'USD', 'fx':  1.00},
-    'CA': {'pop': 36.5, 'gdp':  1.65, 'ccy': 'CAD', 'fx':  1.35},
-    'MX': {'pop':129.2, 'gdp':  1.15, 'ccy': 'MXN', 'fx': 19.68}
+    "US": {"pop": 325.7, "gdp": 19.39, "ccy": "USD", "fx": 1.00},
+    "CA": {"pop": 36.5, "gdp": 1.65, "ccy": "CAD", "fx": 1.35},
+    "MX": {"pop": 129.2, "gdp": 1.15, "ccy": "MXN", "fx": 19.68},
 }
+
 
 def main() -> None:
     running: bool = True
     while running:
-        countryCode: str = input("Please enter a country code: ").upper()
+        countryCode: str = input("Please enter a country code or stop: ").upper()
         if countryCode in allData.keys():
             statistic: str = input("Please enter a statistic: ").lower()
             if statistic in allData[countryCode].keys():
@@ -24,3 +25,8 @@ def main() -> None:
             running = False
         else:
             print("That is an invalid country code.")
+
+
+if __name__ == "__main__":
+    main()
+
