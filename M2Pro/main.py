d00a69f6e5cd2c7a82bbc18312f2ed28b8dad8b5
OttersbachD0987 <ottersbd0987@student.faytechcc.edu>
2025-09-09 12:48:33 -0400

    Stuff And Things

--- M2Pro/main.py
+++ M2Pro/main.py
@@ -1,9 +1,23 @@
 import functions
+import pandas
+import re
 
 
 def Main() -> None:
-    ...
-    ...
+    prices: pandas.DataFrame = pandas.read_csv("SP500_Adjusted_Prices.csv")
+    constituents: pandas.DataFrame = pandas.read_csv(
+        "SP500_Constituents.csv", index_col="Symbol"
+    )
+    usePrices = prices.pivot(index="Symbol", columns="Date", values="Adjusted_price")
+    print(usePrices)
+    merged = constituents.join(usePrices)
+    print(merged)
+    for col in merged:
+        print(col)
+        if re.fullmatch(r"\d{4}-\d{2}-\d{2}", col) is not None:
+            print("Exact Match")
+            print(merged[col])
+            print(merged[merged[col].isna()])
 
 
 if __name__ == "__main__":
