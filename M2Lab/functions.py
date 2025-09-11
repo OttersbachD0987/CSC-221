ef948e4d8e7cd5e6e635400a7f17e36ba094ad33
OttersbachD0987 <ottersbd0987@student.faytechcc.edu>
2025-09-11 10:18:44 -0400

    Success?

--- M2Lab/functions.py
+++ M2Lab/functions.py
@@ -1,2 +1,33 @@
+import pandas
+import numpy
+
+def FindDifference(a_snpData: pandas.DataFrame, a_priceData: pandas.DataFrame) -> pandas.DataFrame:
+    """Adds a differnece column to a pandas dataframe with date columns.
+    
+    Args:
+        a_snpData (DataFrame): A DataFrame containing the full data.
+        a_priceData (DataFrame): A DataFrame containing the price data, dates, and symbols.
+
+    Returns:
+        (DataFrame): The modified DataFrame.
+    """
+    dataHolder: dict[str, list[float]] = {}
+
+    for row in a_snpData.transpose():
+        for col in a_priceData:
+            dataHolder[row] = dataHolder.get(row, []) + ([a_priceData[col][row]] if row in a_priceData[col] and not numpy.isnan(a_priceData[col][row]) else [])
+    
+    a_snpData["difference"] = a_snpData.apply(lambda r: dataHolder[r.name][-1] - dataHolder[r.name][-2] if len(dataHolder[r.name]) > 1 else None, axis=1)
+
+    return a_snpData
 
 
+#### Psuedocode
+## Set dataHolder to {}
+## Foreach row in a_snpData transposed
+##   Foreach col in a_priceData
+##     Set dataHolder[row] to dataHolder get row or default to [] concatenated with a_priceData of row is in a_priceData[col] and a_priceData[col][row] is not NaN, else []
+##   End Foreach
+## End Foreach
+## Set a_snpData["difference"] to the difference between the price on the last available date and the price on the prior available date fo every row
+## Return a_snpData
--- M2Lab/main.py
+++ M2Lab/main.py
@@ -1,34 +1,54 @@
-# A brief description of the project
-# 9/8/2025
+# Load two files, pivot one, join to the other, augment the result by getting the difference between the two last available prices, and print missing values.
+# 9/11/2025
 # CSC221 M2Lab– Panda DF
 # Daley Ottersbach
 
 import functions
 import pandas
-import re
-
 
 def Main() -> None:
+    """Runs the main loop of the program.
+    """
     prices: pandas.DataFrame = pandas.read_csv("SP500_Adjusted_Prices.csv")
     constituents: pandas.DataFrame = pandas.read_csv("SP500_Constituents.csv", index_col="Symbol")
     print("Data frames loaded")
+    print(prices)
+    print(constituents)
     usePrices = prices.pivot(index="Symbol", columns="Date", values="Adjusted_price")
     print("Rotated prices")
+    print(usePrices)
     merged = constituents.join(usePrices)
     print("Joined data")
+    print(merged)
 
     # I don't like doing it like this.
-    bonk: dict[str, list[float]] = {}
+    merged = functions.FindDifference(merged, usePrices)
 
-    for row in merged.transpose():
-        for col in usePrices:
-            bonk[row] = bonk.get(row, []) + ([usePrices[col][row]] if row in usePrices[col] and isinstance(usePrices[col][row], float) else [])
-    
-    merged["difference"] = merged.apply(lambda r: bonk[r.name][-2] - bonk[r.name][-1] if len(bonk[r.name]) > 1 else None, axis=1)
     print("Added difference column")
+    print(merged)
     
     missing = merged[merged.difference.isna()]
     print("Determined Missing Stocks")
+    print(missing)
 
 if __name__ == "__main__":
     Main()
+
+#### Psuedocode
+## Set prices to CSV Content of SP500_Adjusted_Prices.csv
+## Set constituents to CSV Content of SP500_Constituents.csv with index column Symbol
+## Display "Data frames loaded"
+## Display prices
+## Display constituents
+## Set usePrices to prices pivoted with index symbol, columns Date, values Adjusted_price
+## Display "Rotated prices"
+## Display usePrices
+## Set merged to constituents joined with usePrices
+## Display "Joined data"
+## Display merged
+## Set merged to FindDifference with merged, usePrices
+## Display "Added difference column"
+## Display merged
+## Set missing to merged boolean selection of difference column is NaN
+## Display "Joined data"
+## Display missing
